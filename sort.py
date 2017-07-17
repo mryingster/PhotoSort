22cf37650ea1b212357bcfb33e5d9b4a6f05d4a6
Michael Caldwell <michaelc@yingster.net>
2017-07-16 18:34:30 -0700

    Made files array into a dictionary array to store additional information about each file. Added supporting functions for file name extraction and error messages.

--- sort.py
+++ sort.py
@@ -12,6 +12,10 @@ supportedExtensions = [
     "mp4"
 ]
 
+def error(msg):
+    print("ERROR: %s" % msg)
+    quit(1)
+
 def getDateFromEXIF(image):
     import PIL.ExifTags
     import PIL.Image
@@ -41,16 +45,31 @@ def destinationFromDate(date):
 def getExtension(path):
     return path.split(".")[-1].lower()
 
-def moveFile(file, destination):
-    if not os.path.exists(destination):
-        os.makedirs(destination)
+def getBaseName(path):
+    return os.path.basename(path)
 
+def getFilenameWithoutExtension(path):
+    return os.path.splitext(os.path.basename(path))[0]
+
+def moveFile(file):
+    # Create new folder if destination doesn't exist
+    if not os.path.exists(file["dest"]):
+        os.makedirs(file["dest"])
+
+    newFilePath = os.path.join(file["dest"], file['basename'])
+
+    # Check for name collision. Error for now
+    if os.path.exists(newFilePath):
+        print("'%s' already exists in '%s'" % (file["basename"], file["dest"]))
+        return -1
+
+    # Move file
     try:
-        os.rename(file, os.path.join(destination, file))
-        print("%s --> %s" % (file, destination))
+        os.rename(file["path"], newFilePath)
+        print("%s --> %s" % (file["path"], newFilePath))
 
     except:
-        print("Unable to move file '%s' to '%s'." % (file, destination))
+        print("Unable to move file '%s' to '%s'." % (file["basename"], file["dest"]))
         return -1
 
     return 0
@@ -61,23 +80,35 @@ def main(argv):
     # Process Arguments
     for i in argv:
         if os.path.isfile(i) and getExtension(i) in supportedExtensions:
-            files.append(i)
-        # TODO if os.path.isdir(i): add all subdirectories to files list
+            files.append({
+                "path" : i,
+                "ext"  : getExtension(i),
+                "filename" : getFilenameWithoutExtension(i),
+                "basename" : getBaseName(i),
+                "date" : "",
+                "dest" : ""
+            })
 
-    # Process Files
+        # TODO -- add all subdirectories to files list
+        # if os.path.isdir(i):
+
+    # If no files specified, exit
+    if len(files) == 0:
+        error("Please specify files to sort!")
+
+    # Process Files - Try getting date from EXIF data
     for file in files:
-        # Try getting date from EXIF data
-        date = getDateFromEXIF(file)
+        file.update({"date" : getDateFromEXIF(file["path"])})
 
-        # If no EXIF available, use system date
-        if date == 0:
-            date = getDateFromSystem(file)
+    # Process Files - If no EXIF available, use system date
+    for file in files:
+        if file["date"] == 0:
+            file.update({"date" : getDateFromSystem(file["path"])})
 
-        # Use date to determine destination
-        destinationFolder = destinationFromDate(date)
-
-        # Move file
-        moveFile(file, destinationFolder)
+    # Process Files - Determine destination, Move files
+    for file in files:
+        file.update({"dest" : destinationFromDate(file["date"])})
+        moveFile(file)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
